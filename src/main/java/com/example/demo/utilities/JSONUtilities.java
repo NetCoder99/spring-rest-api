6241e80e23150d46703f8f7e769ece4514496580
John Dugger <37913500+NetCoder99@users.noreply.github.com>
2021-07-10 09:31:09 -0700

    Update JSONUtilities.java

--- src/main/java/com/example/demo/utilities/JSONUtilities.java
+++ src/main/java/com/example/demo/utilities/JSONUtilities.java
@@ -1,34 +1,102 @@
 package com.example.demo.utilities;
 
+package com.optum.claims.claims_master_controller.classes;
+
+import java.io.File;
+import java.time.LocalDateTime;
 import java.util.List;
 
-import com.example.demo.models.LoginFields;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.dataformat.avro.AvroFactory;
+import com.fasterxml.jackson.dataformat.avro.AvroSchema;
+import com.fasterxml.jackson.dataformat.avro.schema.AvroSchemaGenerator;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.optum.claims.claims_master_controller.mappers.TimeStampDeserializer;
+import com.optum.claims.claims_master_controller.models.AgendaGroupDefs;
+import com.optum.claims.claims_master_controller.models.ArtifactDetails;
 
-public class JSONUtilities<T> {
+public class JsonUtilities<T> {
 
-	@SuppressWarnings({ "rawtypes" })
-	public Object GetObjFromJson(String jsonString,  Class inpClazz) throws Exception
+	
+	public static String SerializeToJson(Object claim) throws JsonProcessingException
 	{
-		ObjectMapper objectMapper = new ObjectMapper();
-		Object rtnObj = objectMapper.readValue(jsonString, new TypeReference<T>(){});
-		return rtnObj;
+		final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper.writeValueAsString(claim);
+	}	
+
+	public Object CreateFromJsonObj(String request, Class<T> clazz) throws Exception
+	{
+		final ObjectMapper objectMapper = GetJsonMapper();
+		T tObj = clazz.getDeclaredConstructor().newInstance();
+		JavaType type = objectMapper.getTypeFactory().constructType(tObj.getClass());
+
+		SimpleModule module = new SimpleModule();
+		module.addDeserializer(LocalDateTime.class, new TimeStampDeserializer());
+		objectMapper.registerModule(module);
+		
+		tObj = objectMapper.readValue(request, type);    
+	    return tObj;
 	}
 
-	public List<T> GetListFromJson(String jsonString,  Class inpClazz) throws Exception
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	public List<T> CreateListFromJson(String request, Class<T> clazz) throws Exception
 	{
-		ObjectMapper objectMapper = new ObjectMapper();
-		List<T> rtnList = objectMapper.readValue(jsonString, new TypeReference<List<T>>(){});
-		return rtnList;
+		final ObjectMapper objectMapper = GetJsonMapper();
+		T tObj = clazz.getDeclaredConstructor().newInstance();
+		JavaType type = objectMapper.getTypeFactory().constructCollectionType(List.class, tObj.getClass());
+	    List<T> tmpList = objectMapper.readValue(request, type);    
+	    return tmpList;
+	}	
+	
+	public static String CreateAvroFromPojo(Class<?> clazz) throws Exception
+	{
+		ObjectMapper objectMapper = new ObjectMapper(new AvroFactory());
+		AvroSchemaGenerator gen = new AvroSchemaGenerator();
+		objectMapper.acceptJsonFormatVisitor(clazz, gen);
+		AvroSchema schemaWrapper = gen.getGeneratedSchema();
+		org.apache.avro.Schema avroSchema = schemaWrapper.getAvroSchema();
+		return avroSchema.toString(true);
 	}
 
-	public static LoginFields getLoginFields(String jsonString) throws Exception
+	public static List<AgendaGroupDefs> GetAgendaGroups() throws Exception
+	{
+		final ObjectMapper objectMapper  = GetYamlMapper();
+	    ObjectReader objectReader = objectMapper.reader().forType(new TypeReference<List<AgendaGroupDefs>>(){});
+	    List<AgendaGroupDefs> rtnList = objectReader.readValue(new File("src/main/resources/AgendaGroups.yml"));
+	    return rtnList;
+	}
+	
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	public static ObjectMapper GetJsonMapper()
 	{
 		ObjectMapper objectMapper = new ObjectMapper();
-		LoginFields rtnObj = objectMapper.readValue(jsonString, new TypeReference<LoginFields>(){});
-		return rtnObj;
+		objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+	    objectMapper.registerModule(new JavaTimeModule());
+	    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+	    return objectMapper; 
 	}
-
+	
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+	public static ObjectMapper GetYamlMapper()
+	{
+		ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+		objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+	    objectMapper.registerModule(new JavaTimeModule());
+	    objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+	    return objectMapper; 
+	}	
 }
+
